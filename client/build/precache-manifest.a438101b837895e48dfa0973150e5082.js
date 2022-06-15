a07cc7c48235e4f025de415048fd8982cc4f06df
Or1Zuk <84446616+Or1Zuk@users.noreply.github.com>
2022-06-15 22:25:08 +0300

    First commit

---
+++ client/build/precache-manifest.a438101b837895e48dfa0973150e5082.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716844ca16226ed7d84fd5fb2a10f146",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e6acc5d592b7e3ab43d",
+    "url": "/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "efd4c2c314396b44e2f6",
+    "url": "/static/css/main.861d7c09.chunk.css"
+  },
+  {
+    "revision": "3e6acc5d592b7e3ab43d",
+    "url": "/static/js/2.0564d9f0.chunk.js"
+  },
+  {
+    "revision": "9d7baa484de148efea1c408ede69d272",
+    "url": "/static/js/2.0564d9f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efd4c2c314396b44e2f6",
+    "url": "/static/js/main.d775bfc0.chunk.js"
+  },
+  {
+    "revision": "327d8edc162049f533fb",
+    "url": "/static/js/runtime-main.4bef3b12.js"
+  },
+  {
+    "revision": "4b90574469509cd88ea93ba737847427",
+    "url": "/static/media/LogoLight.4b905744.png"
+  },
+  {
+    "revision": "e955dcfa43fa090fb97b3916289b2320",
+    "url": "/static/media/background.e955dcfa.jpeg"
+  },
+  {
+    "revision": "666935f7bf9244cf713a9662efd9242f",
+    "url": "/static/media/edit-icon.666935f7.png"
+  },
+  {
+    "revision": "a667a477af8da742e8ff4dd805224edd",
+    "url": "/static/media/orAviv.a667a477.jpeg"
+  },
+  {
+    "revision": "9159494ed34d47176cfe1769e81c12b6",
+    "url": "/static/media/remove.9159494e.png"
+  },
+  {
+    "revision": "bae6a20afdd17f1d5bf97b0bedad0794",
+    "url": "/static/media/student.bae6a20a.png"
+  },
+  {
+    "revision": "91d7d73e69d9c62a6895e2a6f166eb0f",
+    "url": "/static/media/warning.91d7d73e.png"
+  }
+]);
